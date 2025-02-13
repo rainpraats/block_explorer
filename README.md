59cbfcf8281fcd2f59c247ec85bb507bec7842a3
rainpraats <rainpraats@gmail.com>
2025-02-13 11:17:45 +0100

    added dependencies ethers and ganache.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Blockchain explorer for an assignment in school.
+
+## To run the project:
+
+1. npm run ganache
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const initApp = () => {};
+const initApp = () => {
+  displayCurrentBlock();
+};
+
+const displayCurrentBlock = () => {};
+
+const validateAddressFormat = (address) => {
+  if (
+    address.length !== 42 ||
+    address.slice(0, 2) !== '0x' ||
+    !/^[a-z0-9]+$/i.test(address)
+  ) {
+    displayAddressError('Input did not match any address.');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
